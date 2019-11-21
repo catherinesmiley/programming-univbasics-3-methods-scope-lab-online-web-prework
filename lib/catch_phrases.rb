3d11203c29f3a8f350f3b28beefb8a28ee15e765
Catherine Smiley <catherinesmiley@gmail.com>
2019-11-21 02:31:59 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -8,6 +8,6 @@ def phrase
 end
 
 def toadstool
-  status = 'Thank You Mario! But Our Princess Is In Another Castl'
+  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   puts status
 end
